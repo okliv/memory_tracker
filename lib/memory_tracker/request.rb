e9b414f74a8d06e87e44b4a946cdfcd8c9f87d05
ok <ok@gee.ks.ua>
2014-07-13 18:48:53 +0300

    fixes to make it fly

--- lib/memory_tracker/request.rb
+++ lib/memory_tracker/request.rb
@@ -31,11 +31,11 @@ module MemoryTracker
     # end
 
     def self.rss
-      rss = PS.pid(Process.pid).first.rss / 1024 rescue 1 #* 0.004096
+      rss = PS.pid(Process.pid).first.rss / 1024 #* 0.004096
     end
 
     def self.vsize
-      vsize = PS.pid(Process.pid).first.vsize / 1024 rescue 1
+      vsize = PS.pid(Process.pid).first.vsize / 1024
     end
 
   end
