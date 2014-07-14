8ba55f8f4b034454c585942152e4e769cd729a04
ok <ok@gee.ks.ua>
2014-07-14 03:17:08 +0300

    fixes to make it fly on linux and macOS

--- lib/memory_tracker.rb
+++ lib/memory_tracker.rb
@@ -1,4 +1,7 @@
 require 'memory_tracker/engine' if defined?(Rails)
 require 'memory_tracker/memory_tracker'
-require 'ps' if RUBY_PLATFORM.include?('darwin')
-require 'sys/proctable' if RUBY_PLATFORM.include?('linux')
+if RUBY_PLATFORM.include?('darwin')
+  require 'ps'
+else
+  require 'sys/proctable'
+end
--- lib/memory_tracker/request.rb
+++ lib/memory_tracker/request.rb
@@ -1,6 +1,6 @@
 module MemoryTracker
   class Request
-    include Sys if RUBY_PLATFORM.include?('linux')
+    include Sys unless RUBY_PLATFORM.include?('darwin')
 
     attr_reader :start_gcstat, :end_gcstat
     attr_reader :gcstat_delta
@@ -10,41 +10,35 @@ module MemoryTracker
     def_delegators :@env, :path, :controller, :action
 
     def initialize(env)
-      @env          = env
+      @env = env
       @start_gcstat = GcStat.new(self.class.rss, self.class.vsize)
     end
 
     def close
-      @end_gcstat   = GcStat.new(self.class.rss, self.class.vsize)
+      @end_gcstat = GcStat.new(self.class.rss, self.class.vsize)
       @gcstat_delta = GcStatDelta.new(@start_gcstat, @end_gcstat)
       self
     end
 
     private
 
-    def self.ps_data_provider
-      RUBY_PLATFORM.include?('darwin') ? PS.pid(Process.pid) : ProcTable.ps(Process.pid)
-    end
-
-    def self.ps_data
-      [*ps_data_provider].first
-    end
-
     def self.rss
-      ps_data.rss / 1024 #* 0.004096
+      rss =
+          if RUBY_PLATFORM.include?('darwin')
+            PS.pid(Process.pid).first.rss / 1024
+          else
+            ProcTable.ps(Process.pid).rss * 0.004096
+          end
     end
 
     def self.vsize
-      ps_data.vsize / 1024
+      vsize =
+          if RUBY_PLATFORM.include?('darwin')
+            PS.pid(Process.pid).vsize / 1024
+          else
+            ProcTable.ps(Process.pid).vsize * 0.000001
+          end
     end
-
-    # def self.rss
-    #   rss = ProcTable.ps(Process.pid).rss * 0.004096
-    # end
-    #
-    # def self.vsize
-    #   vsize = ProcTable.ps(Process.pid).vsize * 0.000001
-    # end
 
 
   end
